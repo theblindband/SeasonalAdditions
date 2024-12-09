962ae9f9b93d10e9ac8dfed40660664473dda6c3
2100874 <2100874@student.uwtsd.ac.uk>
2024-12-09 02:39:25 +0000

    SpecialModelTypesMixin Small Changes

--- src/main/java/net/theblindbandit6/seasonaladditions/mixin/SpecialModelTypesMixin.java
+++ src/main/java/net/theblindbandit6/seasonaladditions/mixin/SpecialModelTypesMixin.java
@@ -16,6 +16,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.Calendar;
 import java.util.HashMap;
@@ -26,18 +27,21 @@ import static net.theblindbandit6.seasonaladditions.SeasonalAdditions.LOGGER;
 @Mixin(SpecialModelTypes.class)
 public class SpecialModelTypesMixin {
     @Unique
+    private static final ChestModelRenderer.Unbaked CHRISTMAS_CHEST = new ChestModelRenderer.Unbaked(ChestModelRenderer.CHRISTMAS_ID);
+    @Unique
     private static final ChestModelRenderer.Unbaked TRAPPED_CHRISTMAS_CHEST = new ChestModelRenderer.Unbaked(SeasonalAdditions.TRAPPED_CHRISTMAS_ID);
     @Unique
     private static final Map<Block, SpecialModelRenderer.Unbaked> BLOCK_TO_MODEL_TYPE = ImmutableMap.<Block, SpecialModelRenderer.Unbaked>builder()
+            .put(Blocks.CHEST, new ChestModelRenderer.Unbaked(ChestModelRenderer.NORMAL_ID))
             .put(Blocks.TRAPPED_CHEST, new ChestModelRenderer.Unbaked(ChestModelRenderer.TRAPPED_ID))
             .build();
 
-    @Inject(method = "buildBlockToModelTypeMap", at = @At("TAIL"), cancellable = true)
-    private static void modifyMap(LoadedEntityModels entityModels, CallbackInfoReturnable<Map<Block, SpecialModelRenderer<?>>> cir) {
+    @Inject(method = "buildBlockToModelTypeMap", at = @At("RETURN"), cancellable = true)
+    private static void buildBlockToModelTypeMapMixin(LoadedEntityModels entityModels, CallbackInfoReturnable<Map<Block, SpecialModelRenderer<?>>> cir) {
         Map<Block, SpecialModelRenderer.Unbaked> map = new HashMap<>(BLOCK_TO_MODEL_TYPE);
         if (ChestBlockEntityRenderer.isAroundChristmas()) {
+            map.put(Blocks.CHEST, CHRISTMAS_CHEST);
             map.put(Blocks.TRAPPED_CHEST, TRAPPED_CHRISTMAS_CHEST);
-            LOGGER.info("Testing Mixin.");
         }
 
         ImmutableMap.Builder<Block, SpecialModelRenderer<?>> builder = ImmutableMap.builder();
